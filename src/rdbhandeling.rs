0eea1b0e4e4e235d40f52cee2011ea84d96737d9
TheSlam <32799244+salmmanfred@users.noreply.github.com>
2021-01-16 13:41:30 +0100

    added changeData addData removeData

--- src/rdbhandeling.rs
+++ src/rdbhandeling.rs
@@ -21,22 +21,28 @@ use std::string;
 
 pub fn getVal_D(file: &str) -> Vec<i64>{
     let mut vs: Vec<i64> = vec!(0);
+    //the regex that gets the data
     let match_D = Regex::new(r"(\|(\d*\w*)+_D\|)( ?\d+)").unwrap();
     let fss  = openfile::readFileLines(file);
+    // reads the file then parses it
     for x in fss{
         for cap in match_D.captures_iter(&x) {
             vs.push(cap[3].parse::<i64>().unwrap() as i64);
         }
         
     }
+    //return the data
     return vs;
 
 }
 
 pub fn getVal_ND(file: &str) -> Vec<String>{
+    // starts a new vec
     let mut vecN : Vec<String> = vec!("".to_string());
+    // regex for getting data
     let match_D = Regex::new(r"(\|(\d*\w*)+_D\|)( ?\d+)").unwrap();
     let fss  = openfile::readFileLines(file);
+    // read data and parse it 
     for x in fss{
         for cap in match_D.captures_iter(&x) {
             vecN.push(cap[2].to_string());
@@ -46,6 +52,7 @@ pub fn getVal_ND(file: &str) -> Vec<String>{
     return vecN;
 
 }
+// gets metadata
  pub fn getVal_M(name: &str) -> Vec<String>{
     let mut msv: Vec<String> = vec!("".to_string());
     let match_M = Regex::new(r"(\|(\d*\w*)+_M\|)(( ?\w* *\w*)*)").unwrap();
@@ -65,6 +72,7 @@ pub fn getVal_ND(file: &str) -> Vec<String>{
     return msv;
     
 }
+// gets the name of meta data
 pub fn getVal_MN(name: &str) -> Vec<String>{
     let mut msv: Vec<String> = vec!("".to_string());
     let match_M = Regex::new(r"(\|(\d*\w*)+_M\|)(( ?\w* *\w*)*)").unwrap();
@@ -84,7 +92,7 @@ pub fn getVal_MN(name: &str) -> Vec<String>{
     return msv;
     
 }
-
+// same as the ones before
 pub fn getVal_A(name: &str) -> Vec<Vec<String>>{
     let mut msv: Vec<String> = vec!();
     let mut msvn: Vec<Vec<String>> = vec!(vec!("".to_string()));
@@ -188,3 +196,53 @@ pub fn returner(name: &str) -> dataBook{
 }*/
 
 
+pub fn addData(name: &str,newdata:&str){
+    let mut data = openfile::readFileLines(name);
+    data.push(newdata.to_owned());
+    let mut datax: String = "".to_string();
+    //just adds the data
+    for x in data{
+        let datap = x+"\n";
+        datax += &datap;
+    }
+    let x = openfile::writeFile(name, &datax);
+}
+pub fn changeData(name: &str, newdata:&str, cname:&str){
+    let mut data = openfile::readFileLines(name);
+    data.push(newdata.to_owned());
+    let mut datax: String = "".to_string();
+    let matcs = Regex::new(r"(\|(\d*\w*)+(_M|_D|_A)\|)").unwrap();
+    //reads the data and then changes the data form the dataname
+    for x in data{
+        for cap in matcs.captures_iter(&x) {
+            if &cap[2] == cname{
+                let datap = cap[0].to_string()+&newdata.to_string()+"\n";
+                datax += &datap;
+            }else{
+                let datap: String = x.to_string()+&"\n".to_string();
+                datax += &datap;
+            }
+        }
+        
+    }
+    let x = openfile::writeFile(name, &datax);
+}
+pub fn removeData(name: &str, cname:&str){
+    let mut data = openfile::readFileLines(name);
+    //data.push(newdata.to_owned());
+    let mut datax: String = "".to_string();
+    let matcs = Regex::new(r"(\|(\d*\w*)+(_M|_D|_A)\|)").unwrap();
+    //parses the data and does not add the data back again after
+    for x in data{
+        for cap in matcs.captures_iter(&x) {
+            if &cap[2] == cname{
+               
+            }else{
+                let datap: String = x.to_string()+&"\n".to_string();
+                datax += &datap;
+            }
+        }
+        
+    }
+    let x = openfile::writeFile(name, &datax);
+}
